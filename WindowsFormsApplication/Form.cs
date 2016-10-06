00703fc2e76dfacc0050323474c4eda0bb3e8a57
VsiTeStudent <VsiTeStudent@STUDENT>
2016-10-06 21:13:11 +0200

    dodao sam tipku

--- WindowsFormsApplication/Form.cs
+++ WindowsFormsApplication/Form.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsApplication
         {
             InitializeComponent();
         }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("tekst")
+        }
     }
 }
